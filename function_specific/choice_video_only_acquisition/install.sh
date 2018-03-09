cf52ea7826df2ea0742d756a39f57a5824f5eaf0
Tom O'Connell <toconnel@caltech.edu>
2018-03-08 23:03:03 -0800

    Made icon add to Launcher idempotent.

--- function_specific/choice_video_only_acquisition/install.sh
+++ function_specific/choice_video_only_acquisition/install.sh
@@ -3,11 +3,15 @@
 cp tom_tracking_terminals.desktop ~/.local/share/applications
 
 # TODO also position at bottom?
-# TODO make idempotent
-app='tom_tracking_terminals.desktop'
+app="tom_tracking_terminals.desktop"
 existing_launcher_apps=`gsettings get com.canonical.Unity.Launcher favorites`
-new_launcher_apps=`echo $existing_launcher_apps | sed s/]/", '${app}']"/`
-gsettings set com.canonical.Unity.Launcher favorites "$new_launcher_apps"
+
+# checking if app is in existing_launcher_apps
+if [[ ! $existing_launcher_apps = *"${app}"* ]]; then
+    echo "Adding icon to launcher..."
+    new_launcher_apps=`echo $existing_launcher_apps | sed s/]/", '${app}']"/`
+    gsettings set com.canonical.Unity.Launcher favorites "$new_launcher_apps"
+fi
 
 # TODO provide uninstall
 
