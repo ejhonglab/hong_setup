afc653068ac4ac563ee0f8c0c57c489fa5254668
Tom O'Connell <toconnel@caltech.edu>
2018-07-20 18:31:52 -0700

    Successfully distributing keys. Modified deploy to use keys + ask for sudo pass.

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # TODO include storage after fixing ssh access there
-ansible-playbook lab.yml -i hosts -u lab --ask-pass --limit !storage
+ansible-playbook lab.yml -i hosts -u lab --limit '!storage' -K
+# --ask-pass to set up SSH keys
